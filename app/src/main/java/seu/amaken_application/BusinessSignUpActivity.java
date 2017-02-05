3370d2372d4e3c65fbc3e6df263d1756cf78b3e9
Sondos <altalhi.sondos@gmail.com>
2017-02-05 15:03:27 +0300

    This the business Sign up Activity

--- app/src/main/java/seu/amaken_application/BusinessSignUpActivity.java
+++ app/src/main/java/seu/amaken_application/BusinessSignUpActivity.java
@@ -1,13 +1,64 @@
 package seu.amaken_application;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class BusinessSignUpActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessSignUpActivity  extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_sign_up);
-    }
-}
+                // Spinner element
+                Spinner spinner = (Spinner) findViewById(R.id.spinner);
+                // Spinner click listener
+                spinner.setOnItemSelectedListener(this);
+
+                // Spinner Drop down elements
+                List<String> BuisnessType = new ArrayList<String>();
+                BuisnessType.add("Cafeteria");
+                BuisnessType.add("Resturant");
+                BuisnessType.add("Salon");
+                BuisnessType.add("Park");
+                BuisnessType.add("Hotel");
+                BuisnessType.add("Souvenir Shop");
+                BuisnessType.add(" Museum");
+                BuisnessType.add(" Travel agency");
+
+                // Creating adapter for spinner
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, BuisnessType);
+
+                // Drop down layout style - list view with radio button
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // attaching data adapter to spinner
+                spinner.setAdapter(dataAdapter);
+            }
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // On selecting a spinner item
+                String item = parent.getItemAtPosition(position).toString();
+
+                // Showing selected spinner item
+                Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+            }
+
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        }
+
+
+
