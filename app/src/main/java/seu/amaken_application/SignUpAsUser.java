6467ab23b60dcebafb710999ae91136b0ab06e16
afaf <shoog-alroo7_@hotmail.com>
2017-02-06 16:33:25 +0300

    layout for sign up_user is done

---
+++ app/src/main/java/seu/amaken_application/SignUpAsUser.java
@@ -0,0 +1,17 @@
+package seu.amaken_application;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class SignUpAsUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_as_user);
+    }
+
+}
